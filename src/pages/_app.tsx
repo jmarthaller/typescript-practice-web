42b6b9575e48503ffeceda444e4f4405aa4087b8
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-30 14:24:30 -0700

    configure for CORS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,29 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ColorModeProvider, CSSReset, ThemeProvider } from '@chakra-ui/react'
+import React from 'react'
+import { Provider, createClient } from 'urql'
+
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        {/* Made options in color-mode-provider.d.ts:23 optional with TS ? may have to change later if conflict */}
+        {/* <ColorModeProvider> */}
+          <CSSReset />
+          <Component {...pageProps} />
+        {/* </ColorModeProvider> */}
+      </ThemeProvider>
+    </Provider>
   )
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,18 +3,36 @@ import { Formik, Form } from 'formik';
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from 'urql';
 
 interface registerProps {
 
 }
 
+
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+      errors {
+        field
+        message
+      }
+      user {
+        username
+        id
+      }
+    }
+  }
+`
+
 const Register: React.FC<registerProps> = ({}) => {
+    const [, register] = useMutation(REGISTER_MUTATION)
         return (
             <Wrapper variant="small">
                 <Formik 
                 initialValues={{username: "", password: ""}} 
                 onSubmit={(values) => {
-                    console.log(values);
+                    return register(values);
                 }}>
                     {({isSubmitting}) => (
                         <Form>
