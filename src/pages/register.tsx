5f2cdeab4ae8cc5e2a456477a2b53c2c14b1a95f
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-01 07:07:15 -0700

    Route user to / page post Register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -26,7 +26,7 @@ const Register: React.FC<registerProps> = ({}) => {
                     if (response.data?.register.errors) {
                         setErrors(toErrorMap(response.data.register.errors));
                     } else if (response.data?.register.user) {
-
+                        router.push("/");
                     }
                 }}>
                     {({isSubmitting}) => (
