66d35037cbdd89e71e0e648cc03ded48b0bafb6a
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-01 16:37:18 -0700

    Hook Logout Mutation to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import React from 'react'
 import NextLink from "next/link";
-import { useMeQuery } from '../generated/graphql';
+import { useMeQuery, useLogoutMutation } from '../generated/graphql';
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+    const [{fetching: logoutFetching}, logout] = useLogoutMutation();
     const [{data, fetching}] = useMeQuery();
     let body = null;
 
@@ -28,7 +29,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         body = (
         <Flex>
             <Box color="white" mr={2}>{data.me.username}</Box>
-            <Button variant="link">Logout</Button>
+            <Button onClick={() =>  { logout(); }} isLoading={logoutFetching} variant="link">Logout</Button>
         </Flex>
         )
     }
