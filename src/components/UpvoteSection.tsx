d406e4ba187a5ec133f47e94f1271940d794c5b1
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-03 07:31:54 -0700

    Format UpvoteSection with Prettier

--- src/components/UpvoteSection.tsx
+++ src/components/UpvoteSection.tsx
@@ -4,47 +4,49 @@ import React, { useState } from "react";
 import { PostSnippetFragment, useVoteMutation } from "../generated/graphql";
 
 interface UpvoteSectionProps {
-    post: PostSnippetFragment;
+  post: PostSnippetFragment;
 }
 
-export const UpvoteSection: React.FC<UpvoteSectionProps> = ({post}) => {
-  const [loadingState, setLoadingState] = useState<'upvote-loading' | 'downvote-loading' | 'not-loading'>('not-loading');
+export const UpvoteSection: React.FC<UpvoteSectionProps> = ({ post }) => {
+  const [loadingState, setLoadingState] = useState<
+    "upvote-loading" | "downvote-loading" | "not-loading"
+  >("not-loading");
   const [, vote] = useVoteMutation();
   return (
     <Flex direction="column" justifyContent="center" alignItems="center" mr={4}>
       <IconButton
         onClick={async () => {
-            if (post.voteStatus === 1) {
-              return;
-            }
-            setLoadingState('upvote-loading')
-            await vote({
-                postId: post.id,
-                value: 1,
-            })
-            setLoadingState('not-loading')
+          if (post.voteStatus === 1) {
+            return;
+          }
+          setLoadingState("upvote-loading");
+          await vote({
+            postId: post.id,
+            value: 1,
+          });
+          setLoadingState("not-loading");
         }}
-        colorScheme={post.voteStatus === 1 ? 'green': undefined}
+        colorScheme={post.voteStatus === 1 ? "green" : undefined}
         icon={<ChevronUpIcon />}
-        isLoading={loadingState === 'upvote-loading'}
+        isLoading={loadingState === "upvote-loading"}
         aria-label="Upvote post"
       />
       {post.points}
       <IconButton
         onClick={async () => {
-            if (post.voteStatus === -1) {
-              return;
-            }
-            setLoadingState('downvote-loading')
-            await vote({
-                postId: post.id,
-                value: -1,
-            })
-            setLoadingState('not-loading')
+          if (post.voteStatus === -1) {
+            return;
+          }
+          setLoadingState("downvote-loading");
+          await vote({
+            postId: post.id,
+            value: -1,
+          });
+          setLoadingState("not-loading");
         }}
-        colorScheme={post.voteStatus === -1 ? 'red': undefined}
+        colorScheme={post.voteStatus === -1 ? "red" : undefined}
         icon={<ChevronDownIcon />}
-        isLoading={loadingState === 'downvote-loading'}
+        isLoading={loadingState === "downvote-loading"}
         aria-label="Downvote post"
       />
     </Flex>
