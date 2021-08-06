979e7933ff13d2f50c5f15d37c6bea46755eb494
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-06 09:22:55 -0700

    Format single post page with Prettier

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,35 +1,29 @@
-import { withUrqlClient } from 'next-urql';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { Layout } from '../../components/Layout';
-import { usePostQuery } from '../../generated/graphql';
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { usePostQuery } from "../../generated/graphql";
 // import React from 'react'
-import { createUrqlClient } from '../../utils/createUrqlClient';
-
-
+import { createUrqlClient } from "../../utils/createUrqlClient";
 
 export const Post = ({}) => {
-    const router = useRouter();
-    const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
-    const [{data, fetching}] = usePostQuery({
-        pause: intId === -1,
-        variables: {
-            id: intId
-        },
-    })
-    if (fetching) {
-        return (
-            <Layout>
-                <div>
-                    Loading...
-                </div>
-            </Layout>
-        );
-    }
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+  if (fetching) {
     return (
-        <Layout>{data?.post?.text}</Layout>
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
     );
-}
+  }
+  return <Layout>{data?.post?.text}</Layout>;
+};
 
-
-export default withUrqlClient(createUrqlClient, {ssr: true})(Post);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
