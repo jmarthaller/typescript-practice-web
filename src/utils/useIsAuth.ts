69baf13b497c7a69c545bd906131b356fe4b5693
Jacob Marthaller <marthaller.jr@gmail.com>
2022-07-09 22:31:00 +0300

    reformat useIsAuth util

--- src/utils/useIsAuth.ts
+++ src/utils/useIsAuth.ts
@@ -3,11 +3,11 @@ import { useEffect } from "react";
 import { useMeQuery } from "../generated/graphql";
 
 export const useIsAuth = () => {
-    const [{ data, fetching }] = useMeQuery();
-    const router = useRouter(); 
-    useEffect(() => {
-        if (!fetching && !data?.me) {
-            router.replace("/login?next=" + router.pathname);
-        }
-    }, [fetching, data, router])
-}
+  const [{ data, fetching }] = useMeQuery();
+  const router = useRouter();
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.replace("/login?next=" + router.pathname);
+    }
+  }, [fetching, data, router]);
+};
