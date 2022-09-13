a23dfeff062109d0d3891b89e91d222a2440c37f
Jacob Marthaller <marthaller.jr@gmail.com>
2022-09-13 08:59:49 -0700

    Reformat all pages with Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import React, { useState } from "react";
 import { Layout } from "../components/Layout";
@@ -6,14 +14,14 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 import { UpvoteSection } from "../components/UpvoteSection";
- 
+
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 15, 
+    limit: 15,
     cursor: null as null | string,
-  })
+  });
   const [{ data, fetching }] = usePostsQuery({
-    variables
+    variables,
   });
 
   if (!fetching && !data) {
@@ -35,26 +43,35 @@ const Index = () => {
         ) : (
           <Stack spacing={8}>
             {data!.posts.posts.map((post) => (
-            <Flex key={post.id} p={5} shadow="md" borderWidth="1px">
-              <UpvoteSection post={post} />
-              <Box>
-                <Heading fontSize="xl">{post.title}</Heading> 
-                <Text>Posted by {post.creator.username}</Text>
-                <Text mt={4}>{post.textSnippet}</Text>
-              </Box>
-            </Flex>
-          ))}
+              <Flex key={post.id} p={5} shadow="md" borderWidth="1px">
+                <UpvoteSection post={post} />
+                <Box>
+                  <Heading fontSize="xl">{post.title}</Heading>
+                  <Text>Posted by {post.creator.username}</Text>
+                  <Text mt={4}>{post.textSnippet}</Text>
+                </Box>
+              </Flex>
+            ))}
           </Stack>
         )}
-          </div>
-          {data && data.posts.hasMore ? <Flex>
-            <Button onClick={() => {
+      </div>
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button
+            onClick={() => {
               setVariables({
-                limit: variables.limit, 
+                limit: variables.limit,
                 cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
               });
-            }} isLoading={fetching} m="auto" my={4}>Load More Posts</Button>
-          </Flex> : null}
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={4}
+          >
+            Load More Posts
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
